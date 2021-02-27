9ef3a5a9a48b304203e29d975c4fae7135d879c5
chiradenuwan <chiranjayadenuwan@gmail.com>
2021-02-27 16:52:11 +0530

    connect to db with item add,update,delete

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment.prod';
+import {Item} from '../../module/item/Item';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,17 @@ export class ItemService {
 
   getAllItems(): Observable<any> {
     return this.http.get(environment.baseUrl + '/item/getAll');
+  }
+
+  saveItem(item: any): Observable<any> {
+    return this.http.post(environment.baseUrl + '/item/save', item);
+  }
+
+  updateItem(item: Item): Observable<any> {
+    return this.http.put(environment.baseUrl + `/item/update/${item.id}`, item);
+  }
+
+  deleteItem(id: any): Observable<any> {
+    return this.http.delete(environment.baseUrl + `/item/deleteItem/${id}`);
   }
 }
--- src/app/module/item/item/item.component.ts
+++ src/app/module/item/item/item.component.ts
@@ -3,6 +3,10 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Item} from '../Item';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {ItemService} from '../../../core/services/item.service';
+import {AlertService} from 'ngx-alerts';
+import {Alert} from '../../../../assets/alert';
+// @ts-ignore
+import alertJson from '../../../../assets/alert.json';
 
 @Component({
   selector: 'app-item',
@@ -15,155 +19,10 @@ export class ItemComponent implements OnInit {
     qty: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{0,}(.)[0-9]{0,}$')]),
     unitPrice: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{0,}(.)[0-9]{0,}$')]),
   });
-  allItems: Item[] = [
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 1,
-      name: 'SOAP',
-      qty: 6.0,
-      unit_price: 55.0
-    },
-    {
-      id: 2,
-      name: 'ANCHOR',
-      qty: 6.0,
-      unit_price: 240.0
-    },
-    {
-      id: 3,
-      name: 'WATER BOTTLE',
-      qty: 10.0,
-      unit_price: 100.0
-    },
-    {
-      id: 4,
-      name: 'FLOUR',
-      qty: 10.0,
-      unit_price: 100.0
-    }
-  ];
-
-  constructor(private spinner: NgxSpinnerService, private itemService: ItemService) {
-  }
+  allItems: Item[] = [];
+  alertJson: Alert = alertJson;
+  // tslint:disable-next-line:variable-name
+  item_details: any;
 
   get name(): any {
     return this.itemForm.get('name');
@@ -181,6 +40,14 @@ export class ItemComponent implements OnInit {
     await this.loadAllItems();
   }
 
+  constructor(private spinner: NgxSpinnerService, private itemService: ItemService, private alertService: AlertService) {
+  }
+
+  async cancel(savebtn: HTMLButtonElement): Promise<any> {
+    this.itemForm.reset();
+    savebtn.innerText = 'Save';
+  }
+
   async save(savebtn: HTMLButtonElement): Promise<any> {
     this.itemForm.markAllAsTouched();
 
@@ -191,34 +58,69 @@ export class ItemComponent implements OnInit {
         await this.updateItem(savebtn);
       }
     } else {
-
+      this.alertService.warning(this.alertJson.formValidateError);
     }
-  }
-
-  async cancel(savebtn: HTMLButtonElement): Promise<any> {
-    this.itemForm.reset();
-    savebtn.innerText = 'Save';
   }
 
   async saveItem(savebtn: HTMLButtonElement): Promise<boolean> {
     return new Promise(resolve => {
       console.log(this.itemForm.valid);
+      const item = new Item(this.name.value, this.qty.value, this.unitPrice.value);
+      this.itemService.saveItem(item).subscribe((res: any) => {
+        if (res.message === 'Successfully saved!') {
+          console.log(res);
+          this.alertService.success(res.message);
+          this.allItems.push(res.object);
+          this.cancel(savebtn);
+        } else {
+          this.alertService.danger(res.message);
+        }
+      }, error => {
+        this.alertService.danger(this.alertJson.backendError);
+      });
     });
   }
 
   async updateItem(savebtn: HTMLButtonElement): Promise<boolean> {
     return new Promise(resolve => {
-
+      console.log(this.itemForm.valid);
+      const item = new Item(this.name.value, this.qty.value, this.unitPrice.value, this.item_details?.obj?.id);
+      this.itemService.updateItem(item).subscribe((res: any) => {
+        if (res.message === 'Update Successful!') {
+          console.log(res);
+          this.alertService.success(res.message);
+          this.allItems[this.item_details.index] = (res.object);
+          this.cancel(savebtn);
+        } else {
+          this.alertService.danger(res.message);
+        }
+      }, error => {
+        this.alertService.danger(this.alertJson.backendError);
+      });
     });
   }
 
-  async remove(data: Item): Promise<any> {
-
+  async remove(data: Item, i: number): Promise<any> {
+    console.log(data);
+    this.itemService.deleteItem(data?.id).subscribe((res: any) => {
+      if (res.message === 'Removed Successful!') {
+        console.log(res);
+        this.allItems.splice(i, 1);
+        this.alertService.success(res.message);
+      } else {
+        this.alertService.warning(res.message);
+      }
+    }, error => {
+      console.log(error);
+      this.alertService.danger(this.alertJson.backendError);
+    });
   }
 
   async edit(data: Item, i: number, savebtn: HTMLButtonElement, element: HTMLElement): Promise<boolean> {
     return new Promise(resolve => {
       savebtn.innerText = 'Update';
+      this.item_details = {obj: data, index: i};
+      console.log(data);
       this.name.setValue(data.name);
       this.qty.setValue(data.qty);
       this.unitPrice.setValue(data.unit_price);
@@ -231,8 +133,10 @@ export class ItemComponent implements OnInit {
       await this.spinner.show();
       this.itemService.getAllItems().subscribe((res: any) => {
         console.log(res);
-        this.allItems = res;
+        this.allItems = res.object;
       });
     });
   }
+
+
 }
