6feec67e0356d591c7de7809586aa111e653cbde
chiradenuwan <chiranjayadenuwan@gmail.com>
2021-02-27 16:53:04 +0530

    change nav and side bar with image

--- src/app/module/login/sign-in/sign-in.component.ts
+++ src/app/module/login/sign-in/sign-in.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AlertService} from 'ngx-alerts';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {User} from '../User';
+import {UserService} from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +13,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private router: Router, private userService: UserService, private alertService: AlertService,
+              private spinner: NgxSpinnerService) {
+
+  }
+
+
+  get username(): any {
+    return this.loginForm.get('username');
+  }
+
+  get password(): any {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit(): void {
+  }
+
+  async log(isRemember: boolean): Promise<any> {
+    localStorage.clear();
+    sessionStorage.clear();
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.valid) {
+      const signInObject = {username: this.username.value, password: this.password.value};
+      this.spinner.show();
+      if (await this.login(isRemember, signInObject)) {
+        this.spinner.hide();
+        await this.router.navigate(['/main']);
+      } else {
+        this.spinner.hide();
+      }
+    } else {
+      this.alertService.warning('Please check the form');
+    }
+  }
+
+  login(isRemember: boolean, signInObject: any): Promise<boolean> {
+    return new Promise(resolve => {
+      // @ts-ignore
+      this.userService.signIn(signInObject)
+        .subscribe((res: any) => {
+          console.log(res);
+
+          console.log(isRemember);
+          if (res.code === 200) {
+            const data = new User(res.object.id, res.object.name, res.object.username);
+            if (!isRemember) {
+              localStorage.setItem('user', JSON.stringify(data));
+              localStorage.setItem('token', JSON.stringify(res.message));
+            } else {
+              sessionStorage.setItem('user', JSON.stringify(data));
+              sessionStorage.setItem('token', JSON.stringify(res.message));
+            }
+            resolve(true);
+          } else {
+            this.alertService.danger(res.message);
+            resolve(false);
+          }
+        }, (error1: any) => {
+          console.log(error1);
+          if (error1.error.message === 'Unauthorised') {
+            this.alertService.danger('Username or password incorrect!');
+          } else {
+            this.alertService.danger('Something went wrong!');
+          }
+          resolve(false);
+        });
+    });
   }
 
 }
--- src/app/module/page/main/navbar/navbar.component.ts
+++ src/app/module/page/main/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AppService} from '../../../../core/services/app.service';
+import {Route, Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +14,7 @@ export class NavbarComponent implements OnInit {
   allNotification: any;
   str = 'Message {{i}} Title here ok then here is overiding the text.';
 
-  constructor(private appService: AppService) {
+  constructor(private appService: AppService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -28,4 +29,9 @@ export class NavbarComponent implements OnInit {
     this.appService.toggleSidebar();
   }
 
+  logout(): any {
+    localStorage.clear();
+    sessionStorage.clear();
+    this.router.navigate(['/sign-in']);
+  }
 }
